a311c530b5e34a94f7dda5d228054abf78a75900
wawanneutron <wawanneutron1331@gmail.com>
2024-06-02 21:38:17 +0700

    update component new fix

---
+++ src/views/Home.vue
@@ -0,0 +1,91 @@
+<template>
+  <main class="fixed w-full h-screen bg-black">
+    <Sidebar v-if="!showFullVideo" />
+
+    <section v-if="!showFullVideo">
+      <div
+        class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border"
+      >
+        <div
+          class="absolute z-30 h-[600px] left-[120px] w-[77%] right-0 top-0 bg-gradient-to-r from-black via-black"
+        />
+
+        <MovieDetails v-if="movie" :movie="movie" />
+
+        <video
+          v-if="movie"
+          :src="'/videos/' + movie.name + '.mp4'"
+          autoplay
+          loop
+          class="absolute z-0 h-[600px] right-0 top-0"
+        />
+      </div>
+
+      <div
+        class="fixed z-30 bottom-0 right-0 w-full h-[55%] pl-[120px] overflow-y-auto"
+      >
+        <div
+          class="fixed z-30 bottom-0 right-0 w-full h-[55%] pl-[120px] overflow-y-auto"
+        >
+          <VideoCarousel
+            class="pb-14 pt-14"
+            category="Popular Movies"
+            :movies="movies[0]"
+          />
+          <VideoCarousel
+            class="pb-14"
+            category="Horror Movies"
+            :movies="movies[1]"
+          />
+          <VideoCarousel
+            class="pb-32"
+            category="Featured Movies"
+            :movies="movies[2]"
+          />
+        </div>
+      </div>
+
+      <div
+        class="absolute z-20 h-[70%] left-[120px] w-[100%] right-0 bottom-0 bg-gradient-to-t from-black via-black"
+      />
+    </section>
+
+    <section v-if="showFullVideo">
+      <div
+        @click="showFullVideo = false"
+        class="absolute z-50 p-2 m-4 bg-white bg-opacity-50 rounded-full cursor-pointer"
+      >
+        <ChevronLeft fillColor="#FFFFFF" :size="40" />
+      </div>
+      <video
+        :src="'/videos/' + movie.name + '.mp4'"
+        autoplay
+        loop
+        controls
+        class="absolute z-0 w-[100vw] h-full object-fit"
+      />
+    </section>
+  </main>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import movies from '../movies.json'
+
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue'
+import VideoCarousel from '@/components/VideoCarousel.vue'
+import MovieDetails from '@/components/MovieDetails.vue'
+import Sidebar from '@/components/Sidebar.vue'
+
+import { useMovieStore } from '../stores/movie'
+import { storeToRefs } from 'pinia'
+
+const useMovie = useMovieStore()
+const { movie, showFullVideo } = storeToRefs(useMovie)
+
+let video = ref(null)
+
+onMounted(() => {
+  setTimeout(() => (movie.value = movies[0][0]), 100)
+})
+</script>
